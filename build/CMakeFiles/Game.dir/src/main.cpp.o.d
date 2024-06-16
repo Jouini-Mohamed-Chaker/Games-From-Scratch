15f596f630bb3d864a52f6263f989faa16da95d1
Mohamed Cheker Jouini <JouiniMohamedChaker@proton.me>
2024-06-16 05:01:59 +0100

    First raylib Window

---
+++ build/CMakeFiles/Game.dir/src/main.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/Game.dir/src/main.cpp.o: \
+ /home/chaker/Projects/C++\ Games/src/main.cpp /usr/include/stdc-predef.h \
+ /home/chaker/Projects/C++\ Games/includes/raylib-5.0_linux_amd64/include/raylib.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h
